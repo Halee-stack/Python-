5c28f45bf52883b1348353bf55cdef6dfd51550f
Halee-stack <haleesajj@gmail.com>
2026-06-29 19:19:18 +0000

    basics

---
+++ 0.1_basics_of_python/hello_world.py
@@ -0,0 +1,6 @@
+print("Hello World!")
+
+def name(n):  # def is the keyword used to define a function. A function is a reusable block of code that performs a specific task.
+    print(n)
+
+name("Haleema")
---
+++ 0.1_basics_of_python/name.py
@@ -0,0 +1,3 @@
+from hello_world import name
+name("Kate")# the moment we make changes here or run the code, the new file has appeared in the folder. 
+# When we run a Python file, Python often compiles it into bytecode. It stores that compiled bytecode in the __pycache__ folder. It is just a normal directory that Python creates automatically.

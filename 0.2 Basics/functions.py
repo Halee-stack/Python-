350c6d6a5c84e194cb2aeac4c22dc833e63e45e8
Halee-stack <haleesajj@gmail.com>
2026-07-03 12:40:28 +0000

    functions

---
+++ 0.2 Basics/functions.py
@@ -0,0 +1,12 @@
+# A function is one of the most fundamental concepts in Python. It lets you group a set of instructions together, give them a name, and reuse them whenever needed. Instead of writing the same code multiple times, you write it once inside a function and call it whenever you need it.
+def my_function():
+    print("Hello")
+print("Bye")
+
+# def starts the function definition.
+#The function name should describe what the function does.
+#Parameters are optional inputs to the function.
+#The return statement sends a value back to the caller (optional).
+#Indentation is required in Python to define the function body.
+
+#Functions help make your code organized, reusable, and easier to maintain.
